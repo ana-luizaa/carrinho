4df1a84f019aa659d42c6dc4bafda3a98736f45b
ana-luizaa <analuiza222r@gmail.com>
2024-04-15 15:29:38 -0300

    css

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,21 @@ if(document.readyState=='loading'){
 }
 function ready(){
     var botoesEliminarItem = document.getElementsByClassName('btn-eliminar');
-    for(vari=0; i <  botoesEliminarItem.length;i++){
+    for(var i = 0; i <  botoesEliminarItem.length;i++){
         var button = botoesEliminarItem[i];
         button.addEventListener('click, eliminarItemCarrinho');
-        
+
     }
+}
+
+function eliminarItemCarrito(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    
+
+    atualizarTotalCarrinho();
+}
+function atualizarTotalCarrinho (){
+    var carrinhoconteiner = document.getElementsByClassName('carrinho')[0];
+    var carrinhoItens = carrinhoconteiner.getElementsByClassName
 }
